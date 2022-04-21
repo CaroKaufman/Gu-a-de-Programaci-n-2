9ee1787005a702c36e73290fa75622dba54dc848
CaroKaufman <101335676+CaroKaufman@users.noreply.github.com>
2022-04-21 14:41:50 -0300

    ok
    ok

--- Assets/While_01.cs
+++ Assets/While_01.cs
@@ -2,19 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//int counter = 1;
+//int counterMax = 5;
+//  while (counter<=counterMax)
+//    {
+//      Debug.Log("este mensaje c repetira, esta es la counter " + counter + " vuelta");
+//      counter++;
+//    }
+//Debug.Log("Termino el while");
+
 public class While_01 : MonoBehaviour
 {
-    int counter = 1;
-    int counterMax = 5;
+    
     // Start is called before the first frame update
     void Start()
     {
-        while (counter<=counterMax)
-        {
-            Debug.Log("este mensaje c repetira, esta es la counter " + counter + " vuelta");
-            counter++;
-        }
-        Debug.Log("Termino el while");
+        
     }
 
     // Update is called once per frame
---
+++ Assets/While_02.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Realizar un programa que muestre los números entre dos enteros ingresados por el usuario. Chequear que no sean iguales.
+
+public class While_02 : MonoBehaviour
+{
+    public int num1, num2;
+   
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(num1 < num2)
+        {
+            while (num1<num2)
+            {
+                Debug.Log(num1);
+                num1++;
+                
+            }
+        }
+        else 
+        {
+            while (num1 > num2)
+            {
+                Debug.Log(num2);
+                num2++;
+                
+            }
+        }
+
+         Debug.Log("Los números son iguales");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
