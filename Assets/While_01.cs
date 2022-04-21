855c8dfba0b04fc9f09aeb0466751a92c0c5cb81
CaroKaufman <101335676+CaroKaufman@users.noreply.github.com>
2022-04-21 13:59:07 -0300

    ok
    ok

---
+++ Assets/While_01.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class While_01 : MonoBehaviour
+{
+    int counter = 1;
+    int counterMax = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        while (counter<=counterMax)
+        {
+            Debug.Log("este mensaje c repetira, esta es la counter " + counter + " vuelta");
+            counter++;
+        }
+        Debug.Log("Termino el while");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
